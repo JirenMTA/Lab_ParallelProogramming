9e71d8840610c8678046fc06dc60fd364ed521f3
JirenMTA <91962461+JirenMTA@users.noreply.github.com>
2022-10-22 09:11:56 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -46,29 +46,27 @@ void multiply_non_omp(T** matrix1, T** matrix2, T** result, int size)
 	}
 }
 template <class T>
-void multiply_omp(T** matrix1, T** matrix2, T** result, int size)
+void multiply_omp(T** matrix1, T** matrix2, T** result, int size, int num_thread)
 {
-#pragma omp parallel
-	{
-#pragma omp for
-		for (int row = 0; row < size; row++) {
-			for (int col = 0; col < size; col++) {
-				T sum_result = 0;
-				for (int i = 0; i < size; i++) {
-					sum_result += matrix1[row][i] * matrix2[i][col];
-				}
-				result[row][col] = sum_result;
+	int threads;
+	# pragma omp parallel for num_threads(num_thread)
+	for (int i = 0; i < size; i++)
+		for (int j = 0; j < size; j++)
+		{
+			result[i][j] = 0;
+			for (int k = 0; k < size; k++)
+			{
+				result[i][j] += matrix1[i][k] * matrix2[k][j];
 			}
 		}
-	}
 }
 
 void generate_matrix(std::string file_path)
 {
 	int* matrix1, * matrix2;
-	matrix1 = new int[MAX_SIZE * MAX_SIZE];
-	matrix2 = new int[MAX_SIZE * MAX_SIZE];
-
+	matrix1 = new int [MAX_SIZE*MAX_SIZE];
+	matrix2 = new int [MAX_SIZE*MAX_SIZE];
+	
 	for (int i = 0; i < MAX_SIZE * MAX_SIZE; i++) {
 		matrix1[i] = (rand() % MAX_SIZE + 5);
 		matrix2[i] = (rand() % MAX_SIZE - i - 15);
@@ -81,7 +79,7 @@ void generate_matrix(std::string file_path)
 }
 
 template <class T>
-void read_file_to_matrix(std::string file_path, T** matrix1, T** matrix2)
+void read_file_to_matrix(std::string file_path, T **matrix1, T **matrix2)
 {
 	int temp;
 	bool changer = true;
@@ -123,54 +121,90 @@ void write_result_to_file(std::string file_path, T** result, int size)
 
 
 int main() {
+	printf("Max thread is: %d\n", omp_get_max_threads());
+
+
 	int** matrix1, ** matrix2, ** result;
 
-	matrix1 = new int* [MAX_SIZE];
+	matrix1 = new int*[MAX_SIZE];
 	for (int i = 0; i < MAX_SIZE; i++)
 		matrix1[i] = new int[MAX_SIZE];
 
-	matrix2 = new int* [MAX_SIZE];
+	matrix2 = new int*[MAX_SIZE];
 	for (int i = 0; i < MAX_SIZE; i++)
 		matrix2[i] = new int[MAX_SIZE];
 
-	result = new int* [MAX_SIZE];
+	result = new int*[MAX_SIZE];
 	for (int i = 0; i < MAX_SIZE; i++)
 		result[i] = new int[MAX_SIZE];
 
+	for (int i = 0; i < MAX_SIZE; i++)
+	{
+		for (int j = 0; j < MAX_SIZE; j++)
+		{
+			matrix1[i][j] = i + j;
+			matrix2[i][j] = i - j;
+		}	
+	}
+
+	
 	read_file_to_matrix("DataMatrix.txt", matrix1, matrix2);
 
-	std::vector<int> sizes = { 100, 200, 300, 400 };
+	std::vector<int> sizes = {100, 200, 300, 400};
 	int s = 0;
 
 	std::ofstream file("C:/Users/phamn/source/repos/ExampleParallelLab1/Result.txt");
 	while (s < (int)sizes.size()) {
 		const int size = sizes[s];
-
+		
 		auto start1 = std::chrono::steady_clock::now();
 		multiply_non_omp(matrix1, matrix2, result, size);
 		auto end1 = std::chrono::steady_clock::now();
-		std::cout << ">> Using non-omp: " << endl;		file << ">> Using non-omp: " << endl;
-		std::cout << "\tMatrix's size is: " << size << "x" << size << std::endl;		file << "\tMatrix's size is: " << size << "x" << size << std::endl;
-		std::cout << "\tDef meth\'s time: " << std::chrono::duration<double>(end1 - start1).count() << std::endl;		file << "\tDef meth\'s time: " << std::chrono::duration<double>(end1 - start1).count() << std::endl;
+		std::cout << ">> Using non-omp: " << endl;
+		std::cout << "\tMatrix's size is: " << size << "x" << size << std::endl;
+		std::cout << "\tDef meth\'s time: " << std::chrono::duration<double>(end1 - start1).count() << std::endl;
 		if (size == MAX_SIZE)
 			write_result_to_file("Mul1.txt", result, MAX_SIZE);
 
 		auto start2 = std::chrono::steady_clock::now();
-		multiply_omp(matrix1, matrix2, result, size);
+		multiply_omp(matrix1, matrix2, result, size, 2);
 		auto end2 = std::chrono::steady_clock::now();
-		std::cout << ">> Using omp: " << endl;	file << ">> Using omp: " << endl;
+		std::cout << ">> Using omp 2: " << endl;
 		std::cout << "\tMatrix's size is: " << size << "x" << size << std::endl;	file << "\tMatrix's size is: " << size << "x" << size << std::endl;
 		std::cout << "\tDef meth\'s time: " << std::chrono::duration<double>(end2 - start2).count() << std::endl;		file << "\tDef meth\'s time: " << std::chrono::duration<double>(end2 - start2).count() << std::endl;
+		
+		start2 = std::chrono::steady_clock::now();
+		multiply_omp(matrix1, matrix2, result, size, 4);
+		end2 = std::chrono::steady_clock::now();
+		std::cout << ">> Using omp 4: " << endl;	file << ">> Using omp: " << endl;
+		std::cout << "\tMatrix's size is: " << size << "x" << size << std::endl;	file << "\tMatrix's size is: " << size << "x" << size << std::endl;
+		std::cout << "\tDef meth\'s time: " << std::chrono::duration<double>(end2 - start2).count() << std::endl;		file << "\tDef meth\'s time: " << std::chrono::duration<double>(end2 - start2).count() << std::endl;
+
+		start2 = std::chrono::steady_clock::now();
+		multiply_omp(matrix1, matrix2, result, size, 8);
+		end2 = std::chrono::steady_clock::now();
+		std::cout << ">> Using omp 8: " << endl;	file << ">> Using omp: " << endl;
+		std::cout << "\tMatrix's size is: " << size << "x" << size << std::endl;	file << "\tMatrix's size is: " << size << "x" << size << std::endl;
+		std::cout << "\tDef meth\'s time: " << std::chrono::duration<double>(end2 - start2).count() << std::endl;		file << "\tDef meth\'s time: " << std::chrono::duration<double>(end2 - start2).count() << std::endl;
+
+		start2 = std::chrono::steady_clock::now();
+		multiply_omp(matrix1, matrix2, result, size, 12);
+		end2 = std::chrono::steady_clock::now();
+		std::cout << ">> Using omp 12: " << endl;	file << ">> Using omp: " << endl;
+		std::cout << "\tMatrix's size is: " << size << "x" << size << std::endl;	file << "\tMatrix's size is: " << size << "x" << size << std::endl;
+		std::cout << "\tDef meth\'s time: " << std::chrono::duration<double>(end2 - start2).count() << std::endl;		file << "\tDef meth\'s time: " << std::chrono::duration<double>(end2 - start2).count() << std::endl;
+
+
 		std::cout << "_______________________________" << endl;
 		file << "_______________________________" << endl;
-
-		if (size == MAX_SIZE)
+		
+		if ( size == MAX_SIZE)
 			write_result_to_file("Mul2.txt", result, MAX_SIZE);
-		s++;
+		s++;	
 	}
 	file.close();
-
-
+	
+	
 
 	return 0;
 }
